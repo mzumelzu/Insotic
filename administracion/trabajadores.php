66748d3d353e0875ca3f17679acad29310b760d6
mzumelzu <mzumelzu@insotec.cl>
2011-09-22 15:28:07 -0300

    pot mz

--- administracion/trabajadores.php
+++ administracion/trabajadores.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+/// agregué esto
+//esto más
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
     	<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type" />
@@ -6,6 +9,7 @@
 		<link href="../css/administracion.css" rel="stylesheet" type="text/css" />
 		<?php include_once("properties/propiedades.php")?>
 	</head>
+	// listoco
 	<?php
 		if (isset($_POST['guardar']))
 			{
@@ -135,7 +139,7 @@
 			</fieldset>
 		<br>
 	<fieldset>
-    	<legend class="texto_adm_negrita">Mantenci�n de Trabajadores</legend>
+    	<legend class="texto_adm_negrita">Mantenci�n de Trabajadores</legend>
 			<table width="100%" border="0" cellspacing="2" cellpadding="2">
 				<tr>
 			    	<th bgcolor="#CCCCCC" class="texto_adm_negrita" scope="col">RUT</th>
